a7298a4bd7719d9029d1af3a14b576b1b94cd8f0
hlebs <glebszypula1997@gmail.com>
2021-05-06 14:00:53 +0200

    add config.properties

--- desktop-app/src/main/java/org/openjfx/utilities/database/DatabaseController.java
+++ desktop-app/src/main/java/org/openjfx/utilities/database/DatabaseController.java
@@ -1,35 +1,59 @@
 package org.openjfx.utilities.database;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
 public class DatabaseController {
-    //---------- external
-    private static final String dbURL = "jdbc:postgresql://82.209.251.148:60555/inventory";
-    private static final String dbUser = "inventory_user";
-    private static final String dbPassword = "o5EOEPnCEvqN";
-    //---------- internal
-//    private static final String dbURL = "jdbc:postgresql://localhost:60555/inventory";
-//    private static final String dbUser = "inventory_user";
-//    private static final String dbPassword = "o5EOEPnCEvqN";
-    //---------- my
-//    private static final String dbURL = "jdbc:postgresql://localhost:5432/postgres";
-//    private static final String dbUser = "postgres";
-//    private static final String dbPassword = "postgres";
     protected static Connection conn = connect();
 
     public static Connection connect() {
+        String propFileName = "config.properties";
+
+        String dbURL;
+        String dbUser;
+        String dbPassword;
+
         Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(dbURL, dbUser, dbPassword);
-            System.out.print("Connected to the PostgreSQL server successfully");
-        } catch (SQLException e) {
-            showMessageDialog(null, e.getMessage());
-            System.out.println(e.getMessage());
+
+        try (InputStream inputStream = DatabaseController.class.getClassLoader().getResourceAsStream(propFileName)) {
+            Properties prop = new Properties();
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+
+//            /// EXTERNAL
+//            dbURL = prop.getProperty("externalURL");
+//            dbUser = prop.getProperty("externalUser");
+//            dbPassword = prop.getProperty("externalPassword");
+//            /// INTERNAL
+//            dbURL = prop.getProperty("internalURL");
+//            dbUser = prop.getProperty("internalUser");
+//            dbPassword = prop.getProperty("internalPassword");
+            /// LOCAL
+            dbURL = prop.getProperty("localURL");
+            dbUser = prop.getProperty("localUser");
+            dbPassword = prop.getProperty("localPassword");
+
+            try {
+                conn = DriverManager.getConnection(dbURL, dbUser, dbPassword);
+                System.out.print("Connected to the PostgreSQL server successfully");
+            } catch (SQLException e) {
+                showMessageDialog(null, e.getMessage());
+                System.out.println(e.getMessage());
+            }
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
         }
 
         return conn;
@@ -37,7 +61,7 @@ public class DatabaseController {
 
     public static void psExecute(String sql) throws SQLException {
         try (
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+                PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.execute();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
