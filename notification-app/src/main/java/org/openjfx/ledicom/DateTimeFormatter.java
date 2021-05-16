77034493b8991576b0bf94c1d254ca9a00391f19
hlebs <glebszypula1997@gmail.com>
2021-05-16 20:06:09 +0200

    fixes

--- notification-app/src/main/java/org/openjfx/ledicom/DateTimeFormatter.java
+++
@@ -1,9 +0,0 @@
-package org.openjfx.ledicom;
-
-public class DateTimeFormatter {
-    private static final java.time.format.DateTimeFormatter dateTimeFormatter = java.time.format.DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-    public static java.time.format.DateTimeFormatter getDateTimeFormatter() {
-        return dateTimeFormatter;
-    }
-}
